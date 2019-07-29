9df01728f197c8e9ade297ee4421e7d4a2e103ec
Joao Pedro <joaopedrowatson@hotmail.com>
2019-07-29 12:13:27 -0300

    desafio 3 feito

--- desafio3_fatorial.php
+++ desafio3_fatorial.php
@@ -1,3 +1,12 @@
 <?php
+$input = 5;
+$multiplicando = 1;
+$cont = 1;
+
+while ($cont <= $input){
+    $multiplicando = $multiplicando * $cont;
+    $cont++;
+}
+echo "O resultado do fatorial é $multiplicando"
 
 ?>
